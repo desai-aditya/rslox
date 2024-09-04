4a341c2d6e702030643b5399ec380935140e204c
Aditya Desai <adityadesai@Adityas-MacBook-Air.local>
2024-09-03 17:35:59 -0700

    Add basic error reporting

--- src/lox/mod.rs
+++ src/lox/mod.rs
@@ -12,6 +12,16 @@ impl Lox {
     pub fn new() -> Self {
         Self { had_error: false }
     }
+
+    pub fn error(&mut self, line: u32, message: String) {
+        self.report(line, "".to_string(), message);
+    }
+
+    pub fn report(&mut self, line: u32, location: String, message: String) {
+        eprintln!("[line {}]: Error {}: {}", line, location, message);
+        self.had_error = true;
+    }
+
     pub fn run(&self, source: String) -> io::Result<()> {
         let mut scan = Scanner::new(&source);
         loop {
@@ -25,10 +35,14 @@ impl Lox {
 
     pub fn run_file(self, filename: String) -> io::Result<()> {
         let file_string = read_to_string(filename)?;
-        self.run(file_string)
+        self.run(file_string)?;
+        if self.had_error {
+            exit(-1);
+        }
+        Ok(())
     }
 
-    pub fn run_prompt(self) -> io::Result<()> {
+    pub fn run_prompt(mut self) -> io::Result<()> {
         loop {
             let mut buf: String = "".to_owned();
             print!("> ");
@@ -41,6 +55,7 @@ impl Lox {
                 break;
             }
             self.run(buf)?;
+            self.had_error = false;
         }
         Ok(())
     }
